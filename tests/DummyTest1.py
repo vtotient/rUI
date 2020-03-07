9fb5a6d541bf788683a7dfa53196a0b6731cae40
roxmysox <harrison@mitgangs.com>
2020-03-07 13:33:03 -0800

    Op codes from csv. minimally revamped test class structure.

--- tests/DummyTest1.py
+++ tests/DummyTest1.py
@@ -1,12 +1,12 @@
 from .baseTest import BaseTest
 
 class DummyTest1(BaseTest):
-    name = "dummytest1"
     duration = 1000
     def __init__(self, device, duration=0, arg1="cat", **kwargs):
-        super().__init__(self, device, **kwargs) # Pass arguments that werent explicitly defined to parent class
+        # Pass arguments that werent explicitly defined to parent class
+        # Set up device in parent class
+        super().__init__(device, **kwargs) 
         self.setDuration(duration)
-        self.device = device
     
     def run(self):
         super().run()
@@ -26,7 +26,7 @@ class DummyTest1(BaseTest):
         return self.duration
 
     def __repr__(self):
-        return self.name + " -- duration: " + str(self.duration)
+        return self.name + " -- duration: " + str(self.duration) + " " + hex(self.opcode)
 
     def __str__(self):
         return self.__repr__()
--- tests/baseTest.py
+++ tests/baseTest.py
@@ -1,21 +1,35 @@
+TESTS_DIR = "tests/"
 class BaseTest(object):
     #Define basetest parameters
     name = None
     duration = 1000
+    opcode = 0xFF
 
     # When an __init__ is called on the BaseTest class, a child should be
     # provided to know which test is really being instanciated. `**kwargs` are
     # any arguments that were not explicitly defined by the constructor of both
     # the child class and this class. If these arguments exist, throw an error
     # because unused information was provided.
-    def __init__(self, device, child=None, **kwargs):
-        if len(kwargs) != 0 and child:
-            raise NameError("Unknown arguments for " + child.name, kwargs)
+    def __init__(self, device, **kwargs):
+        # Set name to class' name
+        self.name = self.__class__.__name__
+        if len(kwargs) != 0 and self:
+            raise NameError("Unknown arguments for " + self.name, kwargs)
         self.device = device
+
+        # Get opcode
+        with open(TESTS_DIR+"opcodes.csv") as file:
+            for line in file.readlines():
+                cols = line.split(",")
+                if cols[3] == self.name:
+                    self.opcode = int(cols[2])
+                    break
+
     
     def run(self):
         # Ensure device is plugged in
         print("Kicking off the test")
+        self.device.write8Int(self.opcode) # Send the opcode
 
     def terminate(self):
         raise NotImplementedError()
